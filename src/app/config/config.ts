2ff37c83e4323cbfc20d99b16b1aab576cfb7d20
Maxwell Kenned <kenned123@gmail.com>
2019-11-29 03:50:31 -0300

    fix apiKey google

--- src/app/config/config.ts
+++ src/app/config/config.ts
@@ -2,7 +2,8 @@ const config = {
   baseURL: 'https://swapi.co/api/',
   googleSearch: {
     apiUrl: 'https://www.googleapis.com/customsearch/v1',
-    apiKey: 'AIzaSyBoYefcqTrW83xOldw4vCc0PV5z5IV9wQE',
+    apiKeyP1: 'AIzaSyBoYefcqTrW83x',
+    apiKeyP2: 'Oldw4vCc0PV5z5IV9wQE',
     searchEngineId: '017436513531770746396:awebytasx1q'
   }
 };
--- src/app/shared/services/google-search.service.ts
+++ src/app/shared/services/google-search.service.ts
@@ -2,7 +2,6 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import config from 'src/app/config/config';
-import { map, } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -14,7 +13,7 @@ export class GoogleSearchService {
   async searchImage(search: string) {
     return await this.http.get(config.googleSearch.apiUrl, {
       params: {
-        key: config.googleSearch.apiKey,
+        key: config.googleSearch.apiKeyP1 + config.googleSearch.apiKeyP2,
         cx: config.googleSearch.searchEngineId,
         q: search,
         searchType: 'image',
